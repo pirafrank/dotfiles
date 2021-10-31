cf78576f183d955ac64626c17202b0483c4cb3d9
Francesco Pira <dev@fpira.com>
2021-10-31 13:48:07 +0100

    fix

--- gui_terminals/kitty/setup.sh
+++ gui_terminals/kitty/setup.sh
@@ -2,7 +2,7 @@
 
 if [[ $(uname) != 'Linux' ]]; then
   echo "Sorry, linux only for now"
-  exit(1)
+  exit 1
 fi
 
 # install
@@ -25,5 +25,5 @@ TryExec=/home/$(whoami)/.local/kitty.app/bin/kitty
 Exec=/home/$(whoami)/.local/kitty.app/bin/kitty --config /home/$(whoami)/dotfiles/gui_terminals/kitty/kitty.conf
 Icon=/home/$(whoami)/.local/kitty.app/share/icons/hicolor/256x256/apps/kitty.png
 Categories=System;TerminalEmulator;
-Terminal=false" > $HOME/.local/share/application/kitty.desktop
+Terminal=false" > $HOME/.local/share/applications/kitty.desktop
 
