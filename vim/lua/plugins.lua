bcfb73b2926ecb6b596237275d95644c02ff5f2c
Francesco Pira <dev@fpira.com>
2023-10-11 16:56:57 +0200

    disable language providers I don't use

--- vim/lua/plugins.lua
+++ vim/lua/plugins.lua
@@ -1,3 +1,12 @@
+
+-- disable language provider support I don't use.
+-- plugins are either written in lua or vimscript.
+vim.g.loaded_perl_provider = 0
+vim.g.loaded_ruby_provider = 0
+vim.g.loaded_node_provider = 0
+vim.g.loaded_python_provider = 0
+vim.g.loaded_python3_provider = 0
+
 -- shared options
 local ns = { noremap = true, silent = true }
 
