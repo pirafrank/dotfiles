197c53f7c5f8453a11d934a45bef623db9b92567
Francesco Pira <dev@fpira.com>
2023-10-25 10:36:42 +0200

    fix: clear mouse selection on left click w/o select text + clear selection on right click paste

--- gui_terminals/wezterm/.wezterm.lua
+++ gui_terminals/wezterm/.wezterm.lua
@@ -212,23 +212,33 @@ config.mouse_bindings = {
   {
     event = { Down = { streak = 1, button = 'Right' } },
     mods = 'NONE',
-    action = action.PasteFrom 'Clipboard',
+    action = wezterm.action_callback(function(window, pane)
+      window:perform_action(action.PasteFrom 'Clipboard', pane)
+      local has_selection = window:get_selection_text_for_pane(pane) ~= ''
+      if has_selection then
+        window:perform_action(action.ClearSelection, pane)
+      end
+    end)
   },
   -- Change the default click behavior so that it only selects
   -- text and doesn't open hyperlinks
   {
     event = { Up = { streak = 1, button = 'Left' } },
     mods = 'NONE',
-    action = wezterm.action_callback(function(window, pane)
-        window:perform_action(action.CompleteSelection 'ClipboardAndPrimarySelection', pane)
-        window:perform_action(action.ClearSelection, pane)
-    end),
+    action = action.CompleteSelection 'ClipboardAndPrimarySelection',
   },
   -- ...and disable the 'Down' event
   {
     event = { Down = { streak = 1, button = 'Left' } },
     mods = 'NONE',
-    action = action.Nop,
+    action = wezterm.action_callback(function(window, pane)
+      local has_selection = window:get_selection_text_for_pane(pane) ~= ''
+      if has_selection then
+        window:perform_action(action.ClearSelection, pane)
+      else
+        window:perform_action(action.Nop, pane)
+      end
+    end)
   },
   -- and make CTRL-Click open hyperlinks
   {
