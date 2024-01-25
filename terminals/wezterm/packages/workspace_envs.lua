b008fd509c619bd2331fb6ba2b0b15a6dae2bfb8
Francesco Pira <dev@fpira.com>
2024-01-25 11:13:57 +0100

    parametric wezterm workspace generation

---
+++ terminals/wezterm/packages/workspace_envs.lua
@@ -0,0 +1,17 @@
+----------------
+-- workspaces --
+----------------
+
+return {
+  {
+    name = 'fpiracom',
+    dirs = {
+      '~/Code/fpiracom',
+      '~/Code/fpiracom/_data',
+      '~/Code/fpiracom/_posts',
+      '~/Code/fpiracom/_drafts',
+      '~/Code/fpiracom/_pages',
+      '~/Code/fpiracom/_includes'
+    }
+  }
+}
--- terminals/wezterm/packages/workspaces.lua
+++ terminals/wezterm/packages/workspaces.lua
@@ -4,12 +4,46 @@
 
 local wezterm = require 'wezterm'
 local defaults = require 'defaults'
-local utils = require 'utils'
+local workspace_envs = require 'workspace_envs'
 
 local mux = wezterm.mux
 local default_domain = defaults.default_domain
 
 local workspaces = {}
+
+local function add_workspace_functions(envs)
+  for i, env in ipairs(envs) do
+    local workspaceName = env.name
+    local workspaceDirs = env.dirs
+    -- add function to 'workspaces' var
+    workspaces[workspaceName] = function(args)
+      -- spawn new window with multiple tabs
+      local tab, pane, window = mux.spawn_window {
+        workspace = workspaceName,
+        cwd = workspaceDirs[1],
+        args = args,
+        domain = { DomainName = default_domain },
+      }
+      tab:set_title('root')
+      for j = 2, #workspaceDirs do
+        local dir = workspaceDirs[j]
+        local new_tab, new_pane, new_window = window:spawn_tab {
+          cwd = dir,
+          domain = { DomainName = default_domain },
+        }
+        -- split dir string by '/' and get last
+        local title = dir:match '[^/]+$'
+        new_tab:set_title(title)
+      end
+      -- activate first tab
+      tab:activate()
+    end
+  end
+end
+
+-- ************************************
+-- add custom workspace functions below
+-- ************************************
 
 function workspaces.fuzzypi(args)
   -- Set a workspace for fuzzypi on a current project
@@ -30,13 +64,18 @@ function workspaces.fuzzypi(args)
   build_pane:send_text 'cargo build\n'
 end
 
--- merge workspaces into one table
+-- ***************************
+-- DO NOT EDIT BELOW THIS LINE
+-- ***************************
+
+-- adding known workspace envs
+add_workspace_functions(workspace_envs)
+
 -- check if workspaces_hidden.lua exists:
--- if it does, merge it with workspaces
--- if it doesn't, just return workspaces
-local ws = {}
+-- if it does, add workspaces
+-- if it doesn't, don't do anything
 local ok, hidden = pcall(require, 'workspaces_hidden')
 if ok then
-  ws = utils.merge_tables(workspaces, hidden)
+  add_workspace_functions(hidden)
 end
-return ws
+return workspaces
