c9eb6aa6ce5103fa10c61e8f971c5c513f084696
Francesco Pira <dev@fpira.com>
2024-02-19 23:55:46 +0100

    create .zsh_custom* files in zsh install script

--- zsh/zprezto/zprezto_install.sh
+++ zsh/zprezto/zprezto_install.sh
@@ -12,3 +12,12 @@ ln -s "${ZDOTDIR:-$HOME}"/dotfiles/zsh/zprezto/zsh_user_themes "${ZDOTDIR:-$HOME
 
 # powerlevel10k, installation is done automatically by ~/.zpreztorc
 ln -s "${ZDOTDIR:-$HOME}"/dotfiles/zsh/common/p10k.zsh "${ZDOTDIR:-$HOME}/.p10k.zsh"
+
+# custom functions to load before powerlevel10k
+echo """
+# The content of this file will be read at the top of ~/.zshrc.
+# Put in this file anything you need to load BEFORE Powerlevel10k instant prompt.
+""" > "${$HOME}/.zsh_custom_pre"
+
+# custom function lo load as part of .zshrc
+touch ${HOME}/.zsh_custom
