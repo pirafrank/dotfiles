11bdd9d32a4c81ad3388dae0a49c330529627850
Francesco Pira <dev@fpira.com>
2024-02-23 19:22:34 +0100

    parametric home in fzf files

--- fzf/.fzf.bash
+++ fzf/.fzf.bash
@@ -1,13 +1,13 @@
 # Setup fzf
 # ---------
-if [[ ! "$PATH" == */home/francesco/.fzf/bin* ]]; then
-  export PATH="${PATH:+${PATH}:}/home/francesco/.fzf/bin"
+if [[ ! "$PATH" == *${HOME}/.fzf/bin* ]]; then
+  export PATH="${PATH:+${PATH}:}${HOME}/.fzf/bin"
 fi
 
 # Auto-completion
 # ---------------
-[[ $- == *i* ]] && source "/home/francesco/.fzf/shell/completion.bash" 2> /dev/null
+[[ $- == *i* ]] && source "${HOME}/.fzf/shell/completion.bash" 2> /dev/null
 
 # Key bindings
 # ------------
-source "/home/francesco/.fzf/shell/key-bindings.bash"
+source "${HOME}/.fzf/shell/key-bindings.bash"
--- fzf/.fzf.zsh
+++ fzf/.fzf.zsh
@@ -1,13 +1,13 @@
 # Setup fzf
 # ---------
-if [[ ! "$PATH" == */home/francesco/.fzf/bin* ]]; then
-  export PATH="${PATH:+${PATH}:}/home/francesco/.fzf/bin"
+if [[ ! "$PATH" == *${HOME}/.fzf/bin* ]]; then
+  export PATH="${PATH:+${PATH}:}${HOME}/.fzf/bin"
 fi
 
 # Auto-completion
 # ---------------
-[[ $- == *i* ]] && source "/home/francesco/.fzf/shell/completion.zsh" 2> /dev/null
+[[ $- == *i* ]] && source "${HOME}/.fzf/shell/completion.zsh" 2> /dev/null
 
 # Key bindings
 # ------------
-source "/home/francesco/.fzf/shell/key-bindings.zsh"
+source "${HOME}/.fzf/shell/key-bindings.zsh"
