ec86aca8b4910135f615d92f8700ee8795457457
Francesco Pira <dev@fpira.com>
2025-02-28 22:56:55 +0100

    added rebase alias

--- git/git_config.sh
+++ git/git_config.sh
@@ -11,6 +11,7 @@ git config --global alias.co checkout
 git config --global alias.cp cherry-pick
 git config --global alias.st "status -s"
 git config --global alias.br branch
+git config --global alias.rb rebase
 git config --global alias.cm commit
 git config --global alias.pl pull
 git config --global alias.ps push
