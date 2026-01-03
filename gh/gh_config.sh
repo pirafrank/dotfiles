1d06b6fc419ea1f1d457d1b8a794dfe524e262f1
Francesco Pira <dev@fpira.com>
2026-01-03 17:59:54 +0100

    feat: added gh CLI script to config aliases

---
+++ gh/gh_config.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+# Show link to github.com for a file in current branch using: gh url FILE
+# Supports --branch BRANCH_NAME argument. Defaults to current branch.
+gh alias set url 'browse --no-browser'
+
+echo 'All done!'
+
